Implemented model v2     - additional Poisson input to corI     - all projections fixed-pre-number connections     - weights = distribution replicating experimental PSP distribution  +  scaling (population sizes are downscaled --> upscale projection strenghts) Implemented optimize_ratesv2     - tune the projection scaling factors, so that firing rate distributions approximately replicate experimental distribution

srcSim/optimize_ratesv2.py
@@ -2,20 +2,18 @@ from ANNarchy import *
 import pylab as plt
 from scipy import stats
 import sys
-from model import params, rng
+from model import params, rng, add_scaled_projections
 from extras import getFiringRateDist, lognormalPDF, plot_input_and_raster, addMonitors, startMonitors, getMonitors, generateInputs
 
 """
-    this input current optimization + number of synapses optimization needs:
-        params['optimizeRates'] = 'v1'
+    this weight scaling optimization needs:
+        params['optimizeRates'] = 'v2'
         params['increasingInputs'] = False
-        params['input']='Current'
 """
-if (params['optimizeRates']=='v1' and params['increasingInputs']==False and params['input']=='Current') == False:
-    print('other parameters needed for optimize rates v1')
+if (params['optimizeRates']=='v2' and params['increasingInputs']==False) == False:
+    print('other parameters needed for optimize rates v2')
     print("params['optimizeRates'] =",params['optimizeRates'],"--> v1 ?")
     print("params['increasingInputs'] =",params['increasingInputs'],"--> False ?")
-    print("params['input'] =",params['input'],"--> Current ?")
     quit()
 
 # hyperopt
@@ -26,6 +24,33 @@ from hyperopt.pyll import scope
 from multiprocessing import Process
 import multiprocessing
 
+def generateWeights(name, rng, scale=1.0):
+    """
+        function generates new scaled weights for one projection of model v2
+    """
+    weights = np.array(get_projection(name).w)
+    newWeights = params['weightDist'](rng,scale).get_values(int(weights.shape[0]*weights.shape[1]))
+    newWeights = np.reshape(newWeights, weights.shape).tolist()
+    
+    return newWeights
+    
+
+def scaleProjections(S_INP, S_EI, S_IE, S_II, rng):
+    """
+        generates new scaled weights for the projections of model v2 after compilation
+    """
+    get_projection('inputPop_corEL1').w = generateWeights('inputPop_corEL1', rng, scale=S_INP)
+    get_projection('inputPop_corIL1').w = generateWeights('inputPop_corIL1', rng, scale=S_INP)
+    get_projection('corEL1_corIL1').w   = generateWeights('corEL1_corIL1', rng, scale=S_EI)
+    get_projection('corIL1_corEL1').w   = generateWeights('corIL1_corEL1', rng, scale=S_IE)
+    get_projection('corIL1_corIL1').w   = generateWeights('corIL1_corIL1', rng, scale=S_II)
+
+
+
+### add the projections, scaling factors are adjusted during fitting below
+add_scaled_projections(params['fittedParams']['S_INP'], params['fittedParams']['S_EI'], params['fittedParams']['S_IE'], params['fittedParams']['S_II'], rng)
+
+
 
 ### standard model compilation
 simID=int(sys.argv[1])
@@ -33,39 +58,23 @@ mode=str(sys.argv[2])
 compile('annarchy'+str(simID))
 
 
-def set_number_of_synapses(weights, number):
-    """
-        weights: list (for each post neuron) of lists (list of weights to all pre neurons)
-        
-        For each postneuron (idx 0 in weights) sets random weights to zero, "number" weights are kept. Therefore, each neuron only has "number" effective inputs.
-    """
-    for idx, w in enumerate(weights):
-        ### select "number" random indices in weight vector
-        keep_weights_idx=rng.choice(np.arange(0,len(weights[idx]),1).astype(int),np.amin([len(weights[idx]),number]),replace=False)
-        mask=np.zeros(len(weights[idx]))
-        mask[keep_weights_idx]=1
-        ### multiply weight vector with zero, except the "number" values
-        weights[idx]=list(np.array(weights[idx])*mask.astype(int))
-
-    return weights
 
 ### DEFINE SIMULATOR
-def simulator(fitparams, m_list=[0,0]):
+def simulator(fitparams, rng, m_list=[0,0]):
     """
         fitparams: list
-            fitparams[0]: shift of logNormal distribution for the input current values of CorE
-            fitparams[1]: mean of logNormal distribution for the input current values of CorE
-            fitparams[2]: sigma of logNormal distribution for the input current values of CorE
-            fitparams[3]: general number of input synapses of all neurons
+            fitparams[0]: S_INP for weight scaling
+            fitparams[1]: S_EI for weight scaling
+            fitparams[2]: S_IE for weight scaling
+            fitparams[3]: S_II for weight scaling
             
         m_list: variable to store results, from multiprocessing
     """
     ### should plots be generated?
-    plotting=True
+    plotting=False
 
     ### get params
-    init_offsetVal = generateInputs(fitparams[0],fitparams[1],fitparams[2],params['corE_popsize'],rng)['values']
-    number_of_synapses = int(fitparams[3])
+    S_INP, S_EI, S_IE, S_II = fitparams
 
     ### reset model to compilation state
     reset()
@@ -78,13 +87,11 @@ def simulator(fitparams, m_list=[0,0]):
     mon = addMonitors(monDict,mon)
     
 
-    ### Set input strenght
-    get_population('inputPop').offsetVal=init_offsetVal
+    ### Set input strenght, if function is called multiple times --> different inputs (but same distribution)
+    get_population('inputPop').offsetVal = generateInputs(0,1.2,1.1,params['corE_popsize'],rng)['values']
 
-    ### Set number of synapses in projections = set all weights except number of synapses to zero (non-zero weights=random)
-    get_projection('corEL1_corIL1').w=set_number_of_synapses(get_projection('corEL1_corIL1').w, number_of_synapses)
-    get_projection('corIL1_corEL1').w=set_number_of_synapses(get_projection('corIL1_corEL1').w, number_of_synapses)
-    get_projection('corIL1_corIL1').w=set_number_of_synapses(get_projection('corIL1_corIL1').w, number_of_synapses)
+    ### Set scaled weights of the projections
+    scaleProjections(S_INP, S_EI, S_IE, S_II, rng)
 
     ### simulate
     startMonitors(monDict,mon)
@@ -148,7 +155,7 @@ def simulator(fitparams, m_list=[0,0]):
 
 def run_simulator(fitparams):
     """
-        runs the function simulator with the multiprocessing manager (if function is called sequentially, this stores memory, otherwise same as calling sumulator directly)
+        runs the function simulator with the multiprocessing manager (if function is called sequentially, this stores memory, otherwise same as calling simulator directly)
         
         fitparams: list, for description see function simulator
         return: returns dictionary needed for optimization with hyperopt
@@ -156,14 +163,17 @@ def run_simulator(fitparams):
     manager = multiprocessing.Manager()
     m_list = manager.dict()
 
+    num_rep = 10
+    loss = np.zeros(num_rep)
+    for i in range(num_rep):
+        rng = np.random.default_rng()
+        proc = Process(target=simulator,args=(fitparams,rng,m_list))
+        proc.start()
+        proc.join()
 
-    proc = Process(target=simulator,args=(fitparams,m_list))
-
-    proc.start()
-
-    proc.join()
-
-    loss=m_list[0]
+        loss[i]=m_list[0]
+        
+    loss = np.mean(loss)
     obtained=m_list[1]
     
     return {
@@ -172,8 +182,7 @@ def run_simulator(fitparams):
         'obtained': obtained
         }
         
-### TODO implement run_simulator_parallel function, which averages the loss over mnultiple runs (with different seeds), to mean over the random implementation (input curretn, which synapses are kept)
-        
+       
 def testFit(fitparamsDict):
     """
         fitparamsDict: dictionary with parameters, format = as hyperopt returns fit results
@@ -182,7 +191,7 @@ def testFit(fitparamsDict):
         
         Returns the loss computed in simulator function.
     """
-    return run_simulator([fitparamsDict['shift'],fitparamsDict['mean'],fitparamsDict['sigma'],fitparamsDict['number synapses']])['loss']
+    return run_simulator([fitparamsDict['I_INP'],fitparamsDict['I_EI'],fitparamsDict['I_IE'],fitparamsDict['I_II']])['loss']
 
 
 if mode=='optimize':
@@ -190,24 +199,25 @@ if mode=='optimize':
     best = fmin(
         fn=run_simulator,
         space=[
-            hp.uniform('shift', 40, 150),
-            hp.uniform('mean', 0, 3),
-            hp.uniform('sigma', 0.1, 2),
-            scope.int(hp.quniform('number synapses', 5, 49, q=1))
+            hp.uniform('I_INP', 0.5, 20),
+            hp.uniform('I_EI', 0.5, 20),
+            hp.uniform('I_IE', 0.5, 20),
+            hp.uniform('I_II', 0.5, 20)
         ],
         algo=tpe.suggest,
         max_evals=1000)
     best['loss'] = testFit(best)
     
     ### SAVE OPTIMIZED PARAMS AND LOSS
-    np.save('../dataRaw/optimize_rates_obtainedParams'+str(simID)+'.npy',best)
+    np.save('../dataRaw/optimize_ratesv2_obtainedParams'+str(simID)+'.npy',best)
     
 if mode=='test':
     ### LOAD FITTED PARAMETERS
-    fit=np.load('../dataRaw/optimize_rates_obtainedParams'+str(simID)+'.npy', allow_pickle=True).item()
+    fit=np.load('../dataRaw/optimize_ratesv2_obtainedParams'+str(simID)+'.npy', allow_pickle=True).item()
     """results=0
     for i in range(10):
         result+=testFit(fit)['loss']#TODO would run 10 times exactly the same, should use different rng seeds"""
+    #fit = {'I_INP':1, 'I_EI':1, 'I_IE':1, 'I_II':1,}
     ### PRINT LOSS
     result=testFit(fit)
     print(simID, fit, result)

srcSim/parameters.py
@@ -1,6 +1,6 @@
 from ANNarchy import LogNormal, Uniform
 import numpy as np
-from extras import generateInputs
+from extras import generateInputs, scaledANNarchyLogNormal
 
 rng = np.random.default_rng()
 
@@ -8,16 +8,30 @@ params={}
 ### general ANNarchy params
 params['dt'] = 0.1
 params['num_threads'] = 1
-params['optimizeRates'] = False
+params['optimizeRates'] = ['v1','v1post','v2','v2post'][2]# v1 = optimized input current distribution and number-pre-fix, v2 = optimize weight scalings, post = not fitting but use fitted values
 params['increasingInputs'] = False
-params['input']='Current'
-if params['optimizeRates']:
-    ## just use some values for the parameters... they will be overwritten
+
+if 'v1' in params['optimizeRates']:
+    ## define if Poisson or Current input, current input is fitted in v1
+    params['input']= ['Current','Poisson'][0]
+elif 'v2' in params['optimizeRates']:
+    ## in v2 input is Poisson population
+    params['input'] = 'Poisson'
+
+if params['optimizeRates']=='v1':
+    ## just use some values for the parameters... they will be overwritten during optimization
     params['fittedParams']= {'shift':60, 'mean':1.2, 'sigma':1.1, 'number synapses':20}
-else:
+elif params['optimizeRates']=='v1post':
     ## load fitted parameters
     params['useFit'] = 13
     params['fittedParams'] = np.load('../dataRaw/optimize_rates_obtainedParams'+str(params['useFit'])+'.npy', allow_pickle=True).item()
+elif params['optimizeRates']=='v2':
+    ## just use some values for the parameters... they will be overwritten during optimization
+    params['fittedParams']= {'S_INP':1, 'S_EI':1, 'S_IE':1, 'S_II':1}
+elif params['optimizeRates']=='v2post':
+    ## v2 --> weight scalings were fitted
+    params['useFit'] = 1
+    params['fittedParams'] = np.load('../dataRaw/optimize_ratesv2_obtainedParams'+str(params['useFit'])+'.npy', allow_pickle=True).item()
 
 ### Neuron models
 ## conductance based synapses
@@ -52,16 +66,29 @@ params['input_tau'] = 10000#how many miliseconds to increase input current by th
 
 ### Populations
 params['corE_popsize'] = 200
-if params['input']=='Current':
+if params['input']=='Current' and ('v1' in params['optimizeRates']):
+    # input current distribution
     params['inputPop_init_offsetVal'] = generateInputs(params['fittedParams']['shift'],params['fittedParams']['mean'],params['fittedParams']['sigma'],params['corE_popsize'],rng)['values']
-elif params['input']=='Poisson':
+elif params['input']=='Poisson' and ('v1' in params['optimizeRates']):
+    # firing rates of all poisson neurons, in v1 all poisson input neurons have the same rate
     params['inputPop_init_offsetVal'] = 500
+elif 'v2' in params['optimizeRates']:
+    # v2 --> input = poisson with lognormal distributed firing rates, distribution with mean 1.2 and sigma 1.1, similar to firing rate distributions shown in Buzsaki & Mizuseki 2014
+    params['inputPop_init_offsetVal'] = generateInputs(0,1.2,1.1,params['corE_popsize'],rng)['values']
+
 if params['increasingInputs']:
+    # the input does linearly increase with time t
     params['inputPop_init_increaseVal'] = params['inputPop_init_offsetVal']
 else:
     params['inputPop_init_increaseVal'] = 0
 
 ### Projections
-params['weightDist'] = LogNormal(mu=-1.5, sigma=0.93, max=generateInputs(0,-1.5,0.93,1,rng)['threshold'])
-params['numInputs']  = params['fittedParams']['number synapses']
+params['weightDist'] = scaledANNarchyLogNormal
+
+if 'v1' in params['optimizeRates']:
+    params['numInputs']  = params['fittedParams']['number synapses']
+elif 'v2' in params['optimizeRates']:
+    # if model v2 --> use may possible num-pre-fix for all projections
+    params['numInputs']  = params['corE_popsize']//4 - 1
+
 

srcSim/test.py
@@ -1,223 +1,268 @@
 import numpy as np
 import pylab as plt
+testMode = ['z-score', 'lognormal things'][1]
+if testMode=='z-score':
+    ### Load I_CBF and I_CMRO2 for corE and corI without normalization
+    recordingsB = np.load('../dataRaw/simulations_initialTestofBOLD_recordingsB.npy', allow_pickle=True).item()
+    I_CBF_corE = recordingsB['10;r'][:,0]*(np.arange(1,300001,1)/300000+1)
+    I_CBF_corI = recordingsB['11;r'][:,0]*(np.arange(1,300001,1)/300000+1)
+    I_CMRO2_corE = recordingsB['6;I_CMRO2']
+    I_CMRO2_corI = recordingsB['7;I_CMRO2']
 
+    ### divide I_CMRO2 by (k_CMRO2 / k_CBF), thus get the signals which came from the ACC projections
+    k_CBF = 1/2.46
+    k_CMRO2 = 2*k_CBF
+    I_CMRO2_corE /= k_CMRO2 / k_CBF
+    I_CMRO2_corI /= k_CMRO2 / k_CBF
 
-### Load I_CBF and I_CMRO2 for corE and corI without normalization
-recordingsB = np.load('../dataRaw/simulations_initialTestofBOLD_recordingsB.npy', allow_pickle=True).item()
-I_CBF_corE = recordingsB['10;r'][:,0]*(np.arange(1,300001,1)/300000+1)
-I_CBF_corI = recordingsB['11;r'][:,0]*(np.arange(1,300001,1)/300000+1)
-I_CMRO2_corE = recordingsB['6;I_CMRO2']
-I_CMRO2_corI = recordingsB['7;I_CMRO2']
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(I_CBF_corE, label='I_CBF')
+    plt.plot(I_CMRO2_corE, label='I_CMRO2')
+    plt.subplot(212)
+    plt.plot(I_CBF_corI, label='I_CBF')
+    plt.plot(I_CMRO2_corI, label='I_CMRO2')
+    plt.savefig('test.svg')
 
-### divide I_CMRO2 by (k_CMRO2 / k_CBF), thus get the signals which came from the ACC projections
-k_CBF = 1/2.46
-k_CMRO2 = 2*k_CBF
-I_CMRO2_corE /= k_CMRO2 / k_CBF
-I_CMRO2_corI /= k_CMRO2 / k_CBF
+    def normalizeSignal(signal):
+        mode='z_score + tanh'
+        
 
-plt.figure()
-plt.subplot(211)
-plt.plot(I_CBF_corE, label='I_CBF')
-plt.plot(I_CMRO2_corE, label='I_CMRO2')
-plt.subplot(212)
-plt.plot(I_CBF_corI, label='I_CBF')
-plt.plot(I_CMRO2_corI, label='I_CMRO2')
-plt.savefig('test.svg')
+        mean_signal = np.mean(signal[:100000])
+        std_signal = np.std(signal[:100000])
 
-def normalizeSignal(signal):
-    mode='z_score + tanh'
-    
-
-    mean_signal = np.mean(signal[:100000])
-    std_signal = np.std(signal[:100000])
-
-    ### signal during baseline period
-    signal[:100000] = 0
-    
-    ### signal after baseline period
-    if mode=='tanh':
-        signal[100000:] = np.tanh( np.log10( (signal[100000:] + 0.00000001)/(mean_signal + 0.00000001) ) )
-    if mode=='z_score':
-        if std_signal>0:
-            signal[100000:] = (signal[100000:] - mean_signal)/(std_signal*10)
-        else:
-            signal[100000:] = (signal[100000:] - mean_signal)/10
-    if mode=='z_score + tanh':
-        if std_signal>0:
-            signal[100000:] = np.tanh( (signal[100000:] - mean_signal)/(std_signal*10) )
-        else:
-            signal[100000:] = np.tanh( (signal[100000:] - mean_signal)/10. )
-            
-    return signal
-    
-### calculate normalized I_CBF and I_CMRO2 for corE and corI with first 100000 values (first 10000 ms)
-I_CBF_corE   = normalizeSignal(I_CBF_corE)
-I_CBF_corI   = normalizeSignal(I_CBF_corI)
-I_CMRO2_corE = normalizeSignal(I_CMRO2_corE)
-I_CMRO2_corI = normalizeSignal(I_CMRO2_corI)
-
-
-
-### combine I_CBF from corE and corI and combine I_CMRO2 from corE and corI
-I_CBF_combined = (4/5)*I_CBF_corE + (1/5)*I_CBF_corI
-I_CMRO2_combined = (4/5)*I_CMRO2_corE + (1/5)*I_CMRO2_corI
-
-
-### generate for loop which calculates ODEs of BOLD neuron
-
-if 0:
-    newBoldNeuron = Neuron(
-    parameters = """
-        c_CBF       = 1
-        k_CBF       = 1
-        c_CMRO2     = 1
-        k_CMRO2     = 1
-        ea          = 1.0
-        E_0         = 0.34
-        tau_0       = 0.98
-        alpha       = 0.33
-        V_0         = 0.02
-        v_0         = 40.3
-        TE          = 40/1000.
-        epsilon     = 1
-        r_0         = 25
-    """,
-    equations = """
-        I_CBF           = sum(I_f)                                                     : init=0
-        I_CMRO2         = sum(I_r) * (k_CMRO2 / k_CBF)                                 : init=0
-        1000*dsCBF/dt   = ea * I_CBF - c_CBF * sCBF - k_CBF * (CBF - 1)                : init=0
-        1000*dCBF/dt    = sCBF                                                         : init=1, max=2, min=0
-        1000*dsCMRO2/dt = ea * I_CMRO2 - c_CMRO2 * sCMRO2 - k_CMRO2 * (CMRO2 - 1)      : init=0
-        1000*dCMRO2/dt  = sCMRO2                                                       : init=1, max=2, min=0
-
-        1000*dq/dt      = 1 / tau_0 * (CMRO2 - (q / v) * f_out)                        : init=1
-        1000*dv/dt      = 1 / tau_0 * (CBF - f_out)                                    : init=1
-        f_out           = v**(1 / alpha)                                               : init=1
-
-        k_1             = 4.3 * v_0 * E_0 * TE
-        k_2             = epsilon * r_0 * E_0 * TE
-        k_3             = 1 - epsilon
-        BOLD            = V_0 * (k_1 * (1 - q) + k_2 * (1 - (q / v)) + k_3 * (1 - v))  : init=0
-        r=0
-    """,
-        name = "-",
-        description = "-"
-    )
-
-dt=0.1
-duration=30000
-
-### Define/Initialize parameters, variables
-k_CBF   = 1/2.46
-k_CMRO2 = 2*k_CBF
-c_CBF   = 0.6*np.sqrt(4*k_CBF)
-c_CMRO2 = np.sqrt(4*k_CMRO2)
-ea      = 1.0
-E_0     = 0.34
-tau_0   = 0.98
-alpha   = 0.33
-V_0     = 0.02
-v_0     = 40.3
-TE      = 40/1000.
-epsilon = 1
-r_0     = 25
-k_1     = 4.3 * v_0 * E_0 * TE
-k_2     = epsilon * r_0 * E_0 * TE
-k_3     = 1 - epsilon
-
-I_CBF_init   = 0
-I_CMRO2_init = 0
-sCBF_init    = 0
-CBF_init     = 1
-sCMRO2_init  = 0
-CMRO2_init   = 1
-q_init       = 1
-v_init       = 1
-f_out_init   = 1
-BOLD_init    = 0
-
-I_CBF   = np.ones(int(duration/dt))*I_CBF_init
-I_CMRO2 = np.ones(int(duration/dt))*I_CMRO2_init
-sCBF    = np.ones(int(duration/dt))*sCBF_init
-CBF     = np.ones(int(duration/dt))*CBF_init
-sCMRO2  = np.ones(int(duration/dt))*sCMRO2_init
-CMRO2   = np.ones(int(duration/dt))*CMRO2_init
-q       = np.ones(int(duration/dt))*q_init
-v       = np.ones(int(duration/dt))*v_init
-f_out   = np.ones(int(duration/dt))*f_out_init
-BOLD    = np.ones(int(duration/dt))*BOLD_init
+        ### signal during baseline period
+        signal[:100000] = 0
+        
+        ### signal after baseline period
+        if mode=='tanh':
+            signal[100000:] = np.tanh( np.log10( (signal[100000:] + 0.00000001)/(mean_signal + 0.00000001) ) )
+        if mode=='z_score':
+            if std_signal>0:
+                signal[100000:] = (signal[100000:] - mean_signal)/(std_signal*10)
+            else:
+                signal[100000:] = (signal[100000:] - mean_signal)/10
+        if mode=='z_score + tanh':
+            if std_signal>0:
+                signal[100000:] = np.tanh( (signal[100000:] - mean_signal)/(std_signal*10) )
+            else:
+                signal[100000:] = np.tanh( (signal[100000:] - mean_signal)/10. )
+                
+        return signal
+        
+    ### calculate normalized I_CBF and I_CMRO2 for corE and corI with first 100000 values (first 10000 ms)
+    I_CBF_corE   = normalizeSignal(I_CBF_corE)
+    I_CBF_corI   = normalizeSignal(I_CBF_corI)
+    I_CMRO2_corE = normalizeSignal(I_CMRO2_corE)
+    I_CMRO2_corI = normalizeSignal(I_CMRO2_corI)
 
 
 
-### first timestep
-t=0
-### not DFGs
-I_CBF[t]        = I_CBF_combined[t]
-I_CMRO2[t]      = I_CMRO2_combined[t] * (k_CMRO2 / k_CBF)
-f_out[t]        = v_init**(1 / alpha)
+    ### combine I_CBF from corE and corI and combine I_CMRO2 from corE and corI
+    I_CBF_combined = (4/5)*I_CBF_corE + (1/5)*I_CBF_corI
+    I_CMRO2_combined = (4/5)*I_CMRO2_corE + (1/5)*I_CMRO2_corI
 
-### DFGs
-sCBF[t]         = sCBF_init + dt*(ea * I_CBF[t] - c_CBF * sCBF_init - k_CBF * (CBF_init - 1))/1000.
-CBF[t]          = CBF_init + dt*sCBF_init/1000.
-sCMRO2[t]       = sCMRO2_init + dt*(ea * I_CMRO2[t] - c_CMRO2 * sCMRO2_init - k_CMRO2 * (CMRO2_init - 1))/1000.
-CMRO2[t]        = CMRO2_init + dt*sCMRO2_init/1000.
 
-q[t]            = q_init + dt*(1 / tau_0 * (CMRO2_init - (q_init / v_init) * f_out[t]))/1000.
-v[t]            = v_init + dt*(1 / tau_0 * (CBF_init - f_out[t]))/1000.
+    ### generate for loop which calculates ODEs of BOLD neuron
 
-### all other timesteps
-for t in range(1,int((duration)/dt)):
+    if 0:
+        newBoldNeuron = Neuron(
+        parameters = """
+            c_CBF       = 1
+            k_CBF       = 1
+            c_CMRO2     = 1
+            k_CMRO2     = 1
+            ea          = 1.0
+            E_0         = 0.34
+            tau_0       = 0.98
+            alpha       = 0.33
+            V_0         = 0.02
+            v_0         = 40.3
+            TE          = 40/1000.
+            epsilon     = 1
+            r_0         = 25
+        """,
+        equations = """
+            I_CBF           = sum(I_f)                                                     : init=0
+            I_CMRO2         = sum(I_r) * (k_CMRO2 / k_CBF)                                 : init=0
+            1000*dsCBF/dt   = ea * I_CBF - c_CBF * sCBF - k_CBF * (CBF - 1)                : init=0
+            1000*dCBF/dt    = sCBF                                                         : init=1, max=2, min=0
+            1000*dsCMRO2/dt = ea * I_CMRO2 - c_CMRO2 * sCMRO2 - k_CMRO2 * (CMRO2 - 1)      : init=0
+            1000*dCMRO2/dt  = sCMRO2                                                       : init=1, max=2, min=0
 
+            1000*dq/dt      = 1 / tau_0 * (CMRO2 - (q / v) * f_out)                        : init=1
+            1000*dv/dt      = 1 / tau_0 * (CBF - f_out)                                    : init=1
+            f_out           = v**(1 / alpha)                                               : init=1
+
+            k_1             = 4.3 * v_0 * E_0 * TE
+            k_2             = epsilon * r_0 * E_0 * TE
+            k_3             = 1 - epsilon
+            BOLD            = V_0 * (k_1 * (1 - q) + k_2 * (1 - (q / v)) + k_3 * (1 - v))  : init=0
+            r=0
+        """,
+            name = "-",
+            description = "-"
+        )
+
+    dt=0.1
+    duration=30000
+
+    ### Define/Initialize parameters, variables
+    k_CBF   = 1/2.46
+    k_CMRO2 = 2*k_CBF
+    c_CBF   = 0.6*np.sqrt(4*k_CBF)
+    c_CMRO2 = np.sqrt(4*k_CMRO2)
+    ea      = 1.0
+    E_0     = 0.34
+    tau_0   = 0.98
+    alpha   = 0.33
+    V_0     = 0.02
+    v_0     = 40.3
+    TE      = 40/1000.
+    epsilon = 1
+    r_0     = 25
+    k_1     = 4.3 * v_0 * E_0 * TE
+    k_2     = epsilon * r_0 * E_0 * TE
+    k_3     = 1 - epsilon
+
+    I_CBF_init   = 0
+    I_CMRO2_init = 0
+    sCBF_init    = 0
+    CBF_init     = 1
+    sCMRO2_init  = 0
+    CMRO2_init   = 1
+    q_init       = 1
+    v_init       = 1
+    f_out_init   = 1
+    BOLD_init    = 0
+
+    I_CBF   = np.ones(int(duration/dt))*I_CBF_init
+    I_CMRO2 = np.ones(int(duration/dt))*I_CMRO2_init
+    sCBF    = np.ones(int(duration/dt))*sCBF_init
+    CBF     = np.ones(int(duration/dt))*CBF_init
+    sCMRO2  = np.ones(int(duration/dt))*sCMRO2_init
+    CMRO2   = np.ones(int(duration/dt))*CMRO2_init
+    q       = np.ones(int(duration/dt))*q_init
+    v       = np.ones(int(duration/dt))*v_init
+    f_out   = np.ones(int(duration/dt))*f_out_init
+    BOLD    = np.ones(int(duration/dt))*BOLD_init
+
+
+
+    ### first timestep
+    t=0
     ### not DFGs
     I_CBF[t]        = I_CBF_combined[t]
     I_CMRO2[t]      = I_CMRO2_combined[t] * (k_CMRO2 / k_CBF)
-    f_out[t]        = v[t-1]**(1 / alpha)
-    
+    f_out[t]        = v_init**(1 / alpha)
+
     ### DFGs
-    sCBF[t]         = sCBF[t-1] + dt*(ea * I_CBF[t] - c_CBF * sCBF[t-1] - k_CBF * (CBF[t-1] - 1))/1000.
-    CBF[t]          = CBF[t-1] + dt*sCBF[t-1]/1000.
-    sCMRO2[t]       = sCMRO2[t-1] + dt*(ea * I_CMRO2[t] - c_CMRO2 * sCMRO2[t-1] - k_CMRO2 * (CMRO2[t-1] - 1))/1000.
-    CMRO2[t]        = CMRO2[t-1] + dt*sCMRO2[t-1]/1000.
+    sCBF[t]         = sCBF_init + dt*(ea * I_CBF[t] - c_CBF * sCBF_init - k_CBF * (CBF_init - 1))/1000.
+    CBF[t]          = CBF_init + dt*sCBF_init/1000.
+    sCMRO2[t]       = sCMRO2_init + dt*(ea * I_CMRO2[t] - c_CMRO2 * sCMRO2_init - k_CMRO2 * (CMRO2_init - 1))/1000.
+    CMRO2[t]        = CMRO2_init + dt*sCMRO2_init/1000.
 
-    q[t]            = q[t-1] + dt*(1 / tau_0 * (CMRO2[t-1] - (q[t-1] / v[t-1]) * f_out[t]))/1000.
-    v[t]            = v[t-1] + dt*(1 / tau_0 * (CBF[t-1] - f_out[t]))/1000.
-BOLD = V_0 * (k_1 * (1 - q) + k_2 * (1 - (q / v)) + k_3 * (1 - v))
+    q[t]            = q_init + dt*(1 / tau_0 * (CMRO2_init - (q_init / v_init) * f_out[t]))/1000.
+    v[t]            = v_init + dt*(1 / tau_0 * (CBF_init - f_out[t]))/1000.
+
+    ### all other timesteps
+    for t in range(1,int((duration)/dt)):
+
+        ### not DFGs
+        I_CBF[t]        = I_CBF_combined[t]
+        I_CMRO2[t]      = I_CMRO2_combined[t] * (k_CMRO2 / k_CBF)
+        f_out[t]        = v[t-1]**(1 / alpha)
+        
+        ### DFGs
+        sCBF[t]         = sCBF[t-1] + dt*(ea * I_CBF[t] - c_CBF * sCBF[t-1] - k_CBF * (CBF[t-1] - 1))/1000.
+        CBF[t]          = CBF[t-1] + dt*sCBF[t-1]/1000.
+        sCMRO2[t]       = sCMRO2[t-1] + dt*(ea * I_CMRO2[t] - c_CMRO2 * sCMRO2[t-1] - k_CMRO2 * (CMRO2[t-1] - 1))/1000.
+        CMRO2[t]        = CMRO2[t-1] + dt*sCMRO2[t-1]/1000.
+
+        q[t]            = q[t-1] + dt*(1 / tau_0 * (CMRO2[t-1] - (q[t-1] / v[t-1]) * f_out[t]))/1000.
+        v[t]            = v[t-1] + dt*(1 / tau_0 * (CBF[t-1] - f_out[t]))/1000.
+    BOLD = V_0 * (k_1 * (1 - q) + k_2 * (1 - (q / v)) + k_3 * (1 - v))
 
 
 
 
-### plot BOLD variables
+    ### plot BOLD variables
 
-### load params for x axis and times
-simParams   = np.load('../dataRaw/simulations_initialTestofBOLD_simParams.npy', allow_pickle=True).item()
-times=np.arange(simParams['rampUp']+simParams['dt'],simParams['rampUp']+simParams['simDur']+simParams['dt'],simParams['dt'])
+    ### load params for x axis and times
+    simParams   = np.load('../dataRaw/simulations_initialTestofBOLD_simParams.npy', allow_pickle=True).item()
+    times=np.arange(simParams['rampUp']+simParams['dt'],simParams['rampUp']+simParams['simDur']+simParams['dt'],simParams['dt'])
 
 
-plt.subplot(321)
-plt.plot(I_CBF[:])
-plt.subplot(323)
-plt.plot(I_CMRO2[:])
+    plt.subplot(321)
+    plt.plot(I_CBF[:])
+    plt.subplot(323)
+    plt.plot(I_CMRO2[:])
 
-plt.subplot(322)
-plt.title('Self-defined model')
-plt.plot(times,BOLD)
-plt.xlim(times[0],times[-1])
+    plt.subplot(322)
+    plt.title('Self-defined model')
+    plt.plot(times,BOLD)
+    plt.xlim(times[0],times[-1])
 
-plt.subplot(324)
-plt.title('CBF & CMRO2')
-plt.plot(times,CBF,label='CBF')
-plt.plot(times,CMRO2,label='CMRO2')
-plt.xlim(times[0],times[-1])
-plt.legend()
+    plt.subplot(324)
+    plt.title('CBF & CMRO2')
+    plt.plot(times,CBF,label='CBF')
+    plt.plot(times,CMRO2,label='CMRO2')
+    plt.xlim(times[0],times[-1])
+    plt.legend()
 
-plt.subplot(326)
-plt.title('I_CBF & I_CMRO2')
-plt.plot(times,I_CBF,label='I_CBF '+str(round(np.mean(I_CBF),2)))
-plt.plot(times,I_CMRO2,label='I_CMRO2 '+str(round(np.mean(I_CMRO2),2)))
-plt.xlim(times[0],times[-1])
-plt.legend()
+    plt.subplot(326)
+    plt.title('I_CBF & I_CMRO2')
+    plt.plot(times,I_CBF,label='I_CBF '+str(round(np.mean(I_CBF),2)))
+    plt.plot(times,I_CMRO2,label='I_CMRO2 '+str(round(np.mean(I_CMRO2),2)))
+    plt.xlim(times[0],times[-1])
+    plt.legend()
 
-plt.savefig('test.svg')
+    plt.savefig('test.svg')
+elif testMode=='lognormal things':
+
+    from scipy import signal, stats
+    from extras import lognormalPDF, generateInputs
+    rng = np.random.default_rng()
+
+    ### create a lognormal sample
+    mu=-1.5
+    sigma=0.93
+    sample = rng.lognormal(mean=mu, sigma=sigma, size=100000)
+    
+    ### create the PDF for the distribution
+    x = np.linspace(0,10,1000)
+    PDF = lognormalPDF(x, mu=mu, sigma=sigma)
+    
+    ### scale the sample
+    s=2
+    sample2 = s * sample
+    
+    ### create the scaled PDF
+    """shape,loc,scale = stats.lognorm.fit(sample2)
+    sigma2 = shape
+    mu2    = np.log(scale)
+    PDF2 = lognormalPDF(x, mu=mu2, sigma=sigma2, shift=loc)
+    print(mu, sigma, 0)
+    print(mu2, sigma2, loc)
+    print(mu+np.log(s))"""
+    mu2=mu+np.log(s)
+    PDF2 = lognormalPDF(x, mu=mu2, sigma=sigma)
+    
+    plt.figure()
+
+    ### plot original sample
+    plt.subplot(211)
+    plt.hist(sample[sample<x[-1]], 100, density=True, align='mid')
+    plt.plot(x,PDF)
+    plt.xlim(x[0],x[-1])
+    
+    ### plot scaled sample
+    plt.subplot(212)
+    plt.hist(sample2[sample2<x[-1]], 100, density=True, align='mid')
+    plt.plot(x,PDF2)
+    plt.xlim(x[0],x[-1])
+    
+    plt.savefig('test.svg')
 
 
 