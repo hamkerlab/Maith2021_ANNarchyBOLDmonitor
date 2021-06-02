1b753dff03f6536a68b170c42ecbb1181ab676e4
olmai <oli_maith@gmx.de>
2021-06-02 09:05:31 +0200

    started implement BOLD computation from different sources

--- srcSim/simulations.py
+++ srcSim/simulations.py
@@ -177,6 +177,149 @@ def initialTestofBOLD():
     np.save('../dataRaw/simulations_initialTestofBOLD_recordings.npy',recordings)
     np.save('../dataRaw/simulations_initialTestofBOLD_recordingsB.npy',recordingsB)
     np.save('../dataRaw/simulations_initialTestofBOLD_simParams.npy',simParams)
+    
+    
+    
+### TODO: implement function which computes BOLD signal from different sources
+def BOLDfromDifferentSources():
+    #########################################   IMPORTANT SIMULATION PARAMS   ###########################################
+    simParams={}
+    simParams['dt']=params['dt']
+    simParams['input']=params['input']
+    simParams['rampUp']=1000#ms
+    simParams['simDur']=30000#ms
+    simParams['BOLDbaseline']=2000#ms
+
+
+
+    #########################################   ADD PROJECTIONS IF MODEL v2   ###########################################
+    if 'v2' in params['optimizeRates']:
+        """
+            add the scaled projections of model v2
+        """
+        add_scaled_projections(params['fittedParams']['S_INP'], params['fittedParams']['S_EI'], params['fittedParams']['S_IE'], params['fittedParams']['S_II'], rng)
+
+
+    ###################################################   MONITORS   ####################################################
+    if params['input']=='Current':
+        monDict={'pop;inputPop':['r'],
+                 'pop;corEL1':['syn', 'spike'],
+                 'pop;corIL1':['syn', 'spike']}
+    elif params['input']=='Poisson':
+        monDict={'pop;inputPop':['spike'],
+                 'pop;corEL1':['syn', 'spike'],
+                 'pop;corIL1':['syn', 'spike']}
+    mon={}
+    mon=addMonitors(monDict,mon)
+
+
+
+    #################################################   BOLDMONITORS   ##################################################
+    """
+        Generate BOLD monitors with different input signals.
+        Additionally implement BOLD monitors for the individual populations, without normalization to verify the raw input signals.
+        Attention! For the raw input signals, the BOLD calculation may "explode" --> Adjust BOLD monitor parameters, so that no computation takes place.
+    """
+    norm
+    monB={}
+    ### BOLD monitor which uses single input: all syn input
+    monB['1'] = BoldMonitor(populations=[get_population('corEL1'), get_population('corIL1')],
+                            normalize_input=[simParams['BOLDbaseline'],simParams['BOLDbaseline']],
+                            input_variables="syn",
+                            recorded_variables=["BOLD", "r"])
+    monB['1Eraw'] = BoldMonitor(populations=get_population('corEL1'),
+                                scale_factor=1,
+                                input_variables="syn",
+                                recorded_variables=["r"])
+    monB['1Iraw'] = BoldMonitor(populations=get_population('corIL1'),
+                                scale_factor=1,
+                                input_variables="syn",
+                                recorded_variables=["r"])
+                            
+    ### single input: excitatory syn input
+    monB['2'] = BoldMonitor(populations=[get_population('corEL1'), get_population('corIL1')],
+                            normalize_input=[simParams['BOLDbaseline'],simParams['BOLDbaseline']],
+                            input_variables="g_ampa",
+                            recorded_variables=["BOLD", "r"])
+    monB['2Eraw'] = BoldMonitor(populations=get_population('corEL1'),
+                                scale_factor=1,
+                                input_variables="g_ampa",
+                                recorded_variables=["r"])
+    monB['2Iraw'] = BoldMonitor(populations=get_population('corIL1'),
+                                scale_factor=1,
+                                input_variables="g_ampa",
+                                recorded_variables=["r"])
+    
+    ### TODO single input: firing rate
+    
+    ### TODO two inputs: Buxton (2012, 2014, 2021), excitatory --> CMRO2&CBF, inhibitory --> CBF, use post-synaptic currents as driving signals (they likely cause metabolism)
+    
+    ### TODO two inputs: Buxton + Howarth et al. (2021), firing of interneurons --> CMRO2
+    
+
+    ### GENERATE monDict for BOLDMonitors, to easier start and get the monitors
+    monDictB={'BOLD;1':['BOLD'],
+              'BOLD;2':['BOLD', 'r'],
+              'BOLD;3':['BOLD', 'r'],
+              'BOLD;4':['BOLD', 'r', 'f_in'],
+              'BOLD;5':["I_CBF","I_CMRO2","CBF","CMRO2","BOLD"],
+              'BOLD;6':["I_CBF","I_CMRO2","CBF","CMRO2","BOLD"],
+              'BOLD;7':["I_CBF","I_CMRO2","CBF","CMRO2","BOLD"],
+              'BOLD;8':["I_CBF","I_CMRO2","CBF","CMRO2","BOLD"],
+              'BOLD;9':["I_CBF","I_CMRO2","CBF","CMRO2","BOLD"],
+              'BOLD;10':['BOLD', 'r'],
+              'BOLD;11':['BOLD', 'r']}
+
+
+
+    ####################################################   COMPILE   ####################################################
+    compile()
+
+    ### INITIALIZE PARAMETERS OF OWN BOLD MODEL, kCBF from Friston
+    kCBF = 1/2.46
+    kCMRO2 = 2*kCBF
+    monB['5'].k_CBF=kCBF
+    monB['5'].k_CMRO2=kCMRO2
+    monB['5'].c_CBF=0.6*np.sqrt(4*kCBF)
+    monB['5'].c_CMRO2=np.sqrt(4*kCMRO2)
+    
+
+
+    ##################################################   SIMULATION   ###################################################
+
+
+    ### RAMP_UP FOR MODEL
+    simulate(simParams['rampUp'])
+
+
+    ### START MONITORS
+
+    ## standard monitors
+    startMonitors(monDict,mon)
+
+    ## BOLD monitors
+    startMonitors(monDictB,monB)
+
+
+    ### ACTUAL SIMULATION
+    simulate(simParams['simDur'])
+
+
+    ### GET MONITORS
+
+    ## standard monitors
+    recordings={}
+    recordings=getMonitors(monDict,mon,recordings)
+
+    ## BOLD monitors
+    recordingsB={}
+    recordingsB=getMonitors(monDictB,monB,recordingsB)
+    
+
+    ### SAVE DATA
+    np.save('../dataRaw/simulations_initialTestofBOLD_recordings.npy',recordings)
+    np.save('../dataRaw/simulations_initialTestofBOLD_recordingsB.npy',recordingsB)
+    np.save('../dataRaw/simulations_initialTestofBOLD_simParams.npy',simParams)
 
 
 if __name__=='__main__':
