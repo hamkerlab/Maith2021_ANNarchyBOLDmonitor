ba03bf1a57eebf738188f7f0f22e3b902c7a6e21
olmai <oli_maith@gmx.de>
2021-06-02 08:23:25 +0200

    updated optimize_rates --> optimize_ratesv2 in run_parallel

--- srcSim/run_parallel.sh
+++ srcSim/run_parallel.sh
@@ -1,10 +1,10 @@
-python optimize_rates.py 1 optimize &
-python optimize_rates.py 2 optimize &
-python optimize_rates.py 3 optimize &
-python optimize_rates.py 4 optimize &
-python optimize_rates.py 5 optimize &
-python optimize_rates.py 6 optimize &
-python optimize_rates.py 7 optimize &
-python optimize_rates.py 8 optimize &
-python optimize_rates.py 9 optimize &
-python optimize_rates.py 10 optimize
+python optimize_ratesv2.py 1 optimize &
+python optimize_ratesv2.py 2 optimize &
+python optimize_ratesv2.py 3 optimize &
+python optimize_ratesv2.py 4 optimize &
+python optimize_ratesv2.py 5 optimize &
+python optimize_ratesv2.py 6 optimize &
+python optimize_ratesv2.py 7 optimize &
+python optimize_ratesv2.py 8 optimize &
+python optimize_ratesv2.py 9 optimize &
+python optimize_ratesv2.py 10 optimize
