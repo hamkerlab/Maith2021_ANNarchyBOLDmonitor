bada844375f47e7cbfbf55477136e825747f8e38
Helge Dinkelbach <helge.dinkelbach@gmail.com>
2021-11-10 11:25:45 +0100

    Adjusted legend position, axis-label fonts.

--- srcSim/BOLD_comp_time.py
+++ srcSim/BOLD_comp_time.py
@@ -14,7 +14,7 @@ General comments:
 In advance you need to run a simulation with/without a BOLD recording, e. g.
 
 python simulations.py 1 0 1 1 # without
-python simulations.py 1 0 1 2 # without
+python simulations.py 1 0 1 2 # with
 
 In order to modify the number of neurons, you need to adjust the parameters.py:
 
@@ -54,32 +54,14 @@ for idx_n, n in enumerate(neuron_number):
 
 #
 # Plot computation as a function of the number of neurons
+# and additionally the fraction as percent
 #
 from matplotlib.pylab import *
 cm = 1/2.54  # centimeters to inches
 rcParams['font.size'] = 8
 
 fig, ax1 = plt.subplots(1,1,figsize=(20*cm,9*cm), dpi=300)
-plt.subplots_adjust(top=0.95, bottom=0.13, left=0.08)
-
-ax1.errorbar(numpy.arange(len(neuron_number)), w_bold_mean, yerr=w_bold_std, label="with BOLD recording")
-ax1.errorbar(numpy.arange(len(neuron_number)), wo_bold_mean, yerr=wo_bold_std, label="without BOLD recording")
-ax1.set_yscale("log")
-ax1.set_ylabel("computation time [s, log-scale]")
-ax1.set_xticks(np.arange(len(neuron_number)))
-ax1.set_xticklabels([ str(round(x/1000.0,2)) for x in labels])
-ax1.set_xlabel("number of recorded neurons [thousands]")
-ax1.grid(True)
-ax1.legend()
-
-fig.savefig("sim_time_compare_"+str(number_of_inputs)+".png")
-
-#
-# Plot computation as a function of the number of neurons
-# and additionally the fraction as percent
-#
-fig, ax1 = plt.subplots(1,1,figsize=(20*cm,9*cm), dpi=300)
-plt.subplots_adjust(top=0.95, bottom=0.13, left=0.08)
+plt.subplots_adjust(top=0.97, bottom=0.2, left=0.08, right=0.93)
 ax2 = ax1.twinx() 
 
 ax1.errorbar(numpy.arange(len(neuron_number)), w_bold_mean, yerr=w_bold_std, label="with BOLD recording")
@@ -88,14 +70,16 @@ ax1.set_yscale("log")
 # create a more readable y-axis
 ax1.set_yticks([10**x for x in [0, 0.5, 1.0, 1.5, 2.0, 2.5]])
 ax1.set_yticklabels([round(10**x,1) for x in [0, 0.5, 1.0, 1.5, 2.0, 2.5]])
-ax1.set_ylabel("computation time [s, log-scale]")
+ax1.set_ylabel("computation time [s, log-scale]", fontweight="bold")
 ax1.set_xticks(np.arange(len(neuron_number)))
 ax1.set_xticklabels([ str(round(x/1000.0,2)) for x in labels])
-ax1.set_xlabel("number of recorded neurons [thousands]")
+ax1.set_xlabel("number of recorded neurons [thousands]", fontweight="bold")
 ax1.grid(True)
-ax1.legend()
+ax1.legend(ncol=2, bbox_to_anchor=(0.75,-0.15))
 
 ax2.bar(numpy.arange(len(neuron_number)), ((w_bold_mean/wo_bold_mean)-1.0) * 100, width=0.2, color="gray", alpha=0.4)
-ax2.set_ylabel("Fraction of computation time for BOLD recording [%]", color="gray")
+ax2.set_ylabel("Fraction of computation time for\nBOLD recording [%]", color="gray", fontweight="bold")
+ax2.tick_params(axis='y', colors='gray')
 
-fig.savefig("sim_time_compare_"+str(number_of_inputs)+"_frac.png")
+fig.savefig("../results/Fig_comp_time/FigS2.svg")
+fig.savefig("../results/Fig_comp_time/FigS2.png")
